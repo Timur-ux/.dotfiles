e4e21cbccc159df141f24517824fe7a4ac94c026
Tias <timur.buchkin@mail.ru>
2026-03-15 13:52:23 +0300

    add zathura config

--- .config/scripts/zellij-start.sh
+++ .config/scripts/zellij-start.sh
@@ -4,11 +4,11 @@ ZJ_SESSIONS=$(zellij list-sessions -ns)
 NO_SESSIONS=$(echo "${ZJ_SESSIONS}" | wc -l)
 session="$(echo -e "${ZJ_SESSIONS}" | sk)"
 if [[ -z "$session" ]]; then
-	read -p "Input new session name(random by default): " session
+	read -p "Input new session name: " session
 fi
 
 if [[ -z "$session" ]]; then
-	zellij attach -c
+	zellij
 else
 	zellij attach -c "$session"
 fi
